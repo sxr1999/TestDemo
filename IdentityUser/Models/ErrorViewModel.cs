3e5881f4333436cd82ad3a9739b59eb65bc4bc51
欣然沈 <83857035+sxr1999@users.noreply.github.com>
2023-03-30 17:46:52 +0800

    Delete IdentityUser directory

--- IdentityUser/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace IdentityUser.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
