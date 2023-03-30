c4181ad9b500eca3bde70d2ec2741ac53b4228de
欣然沈 <83857035+sxr1999@users.noreply.github.com>
2023-03-30 17:46:36 +0800

    Delete Identity directory

--- Identity/Models/WebApplicationUser.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace Identity.Models
-{
-    public class WebApplicationUser : IdentityUser
-    {
-        public virtual ICollection<Favorite> Favorites { get; set; }
-    }
-}
